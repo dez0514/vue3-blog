36b395db06e4d6e723436f7ea74177010963c43d
zhangwangde <2573433803@qq.com>
2022-12-16 21:10:04 +0800

    dog

--- src/components/vfooter.vue
+++ src/components/vfooter.vue
@@ -3,11 +3,13 @@
     <!-- <div class="visitor">博客已运行：00天00小时00分00秒</div> -->
     <div class="copy-right">保留版权作者 Wintermelon</div>
     <div class="bn-num"><span class="bspan">昨天太近、明天太远</span>鄂ICP备2020019488号-1</div>
+    <div class="dog"><img src="../assets/images/dog.gif" alt=""></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .footer {
+  position: relative;
   box-sizing: border-box;
   height: 138px;
   background-color: var(--gray_8);
@@ -39,11 +41,23 @@
   display: inline-block;
   margin-right: 10px;
 }
-
+.dog {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  img {
+    display: block;
+    max-height: 100%;
+    max-width: 100%;
+  }
+}
 @media screen and (max-width: 990px) {
   .footer {
     height: 60px;
     padding-top: 15px;
+  }
+  .dog {
+    display: none;
   }
   .bn-num {
     margin-top: 5px;
